9a430b1afc3e91621c8e93a64bbad1208e9b59bc
wlgns1501 <wlgns1501@gmail.com>
2021-12-28 20:40:41 +0900

    SEB 3주차2일차

---
+++ twittler_files/script.js
@@ -0,0 +1,31 @@
+// your code here
+
+// DATA는 이미 작성된 트윗을 표시합니다.
+console.log(DATA);
+
+// generateNewTweet을 호출할 때마다 새로운 트윗을 생성합니다.
+console.log(generateNewTweet());
+
+// document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
+document.getElementByClassName("user_info").innerHTML = DATA[0]["user"];
+// document.getElementById("message_info").innerHTML = DATA[0]["message"];
+// document.getElementById("created_info").innerHTML = DATA[0]["created_at"];
+
+// document.getElementById("user_info2").innerHTML = DATA[1]["user"];
+// document.getElementById("message_info2").innerHTML = DATA[1]["message"];
+// document.getElementById("created_info2").innerHTML = DATA[1]["created_at"];
+
+// document.getElementById("user_info3").innerHTML = DATA[2]["user"];
+// document.getElementById("message_info3").innerHTML = DATA[2]["message"];
+// document.getElementById("created_info3").innerHTML = DATA[2]["created_at"];
+
+// document.getElementById("user_info4").innerHTML = DATA[3]["user"];
+// document.getElementById("message_info4").innerHTML = DATA[3]["message"];
+// document.getElementById("created_info4").innerHTML = DATA[3]["created_at"];
+
+// document.getElementById("user_info5").innerHTML = DATA[4]["user"];
+// document.getElementById("message_info5").innerHTML = DATA[4]["message"];
+// document.getElementById("created_info5").innerHTML = DATA[4]["created_at"];
+// 유저 이름
+// 메세지 이름
+// 생성 날짜
